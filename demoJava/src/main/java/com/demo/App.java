f476c4dd0317d27fce836d3bd6e686196d1125fb
lejaka <tklejaka@gmail.com>
2019-09-12 10:08:43 +0200

    Basic java project

---
+++ demoJava/src/main/java/com/demo/App.java
@@ -0,0 +1,18 @@
+package com.demo;
+import com.demo.files;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        files newFilesClass = new files();
+        newFilesClass.Getfiles();
+    }
+
+}
+
+
---
+++ demoJava/src/main/java/com/demo/files.java
@@ -0,0 +1,27 @@
+package com.demo;
+import java.io.File;
+import java.io.IOException;
+
+public class files {
+    public void Getfiles(){
+        File myObj = new File(".../Desktop/New folder/filename.txt");
+        String commmand = " shutdown -i";
+        if(myObj.exists()){
+            System.out.println("File name: " + myObj.getName());
+            System.out.println("Absolute path: " + myObj.getAbsolutePath());
+            System.out.println("Writeable: " + myObj.canWrite());
+            System.out.println("Readable " + myObj.canRead());
+            System.out.println("File size in bytes " + myObj.length());
+        }
+        else {
+            System.out.println("The file does not exist.");
+            try {
+                Process p = Runtime.getRuntime().exec(commmand);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        }
+    }
+
+}
